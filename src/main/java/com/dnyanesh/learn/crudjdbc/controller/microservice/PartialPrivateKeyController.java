9934a05bbd7d7d420ff55f54165b51c3fa88a176
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-04-01 21:32:59 +0530

    🌳🌳 Additing  hash1 and sha-  256. 🌳🌳

--- src/main/java/com/dnyanesh/learn/crudjdbc/controller/microservice/PartialPrivateKeyController.java
+++ src/main/java/com/dnyanesh/learn/crudjdbc/controller/microservice/PartialPrivateKeyController.java
@@ -1,9 +1,14 @@
 package com.dnyanesh.learn.crudjdbc.controller.microservice;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest; 
+import java.security.NoSuchAlgorithmException; 
+  
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 import com.dnyanesh.learn.crudjdbc.model.microservice.senderobject.PartialPrivateKey;
@@ -22,6 +27,48 @@ import it.unisa.dia.gas.plaf.jpbc.util.io.Base64;
 @RestController
 @RequestMapping("/microservice/clpeks")
 public class PartialPrivateKeyController {
+            public static byte[] getSHA(String input) throws NoSuchAlgorithmException
+            { 
+                // Static getInstance method is called with hashing SHA 
+                MessageDigest md = MessageDigest.getInstance("SHA-256"); 
+
+                // digest() method called 
+                // to calculate message digest of an input 
+                // and return array of byte
+                return md.digest(input.getBytes(StandardCharsets.UTF_8)); 
+            }
+            
+            public static String toHexString(byte[] hash)
+            {
+                // Convert byte array into signum representation 
+                BigInteger number = new BigInteger(1, hash); 
+
+                // Convert message digest into hex value 
+                StringBuilder hexString = new StringBuilder(number.toString(16)); 
+
+                // Pad with leading zeros
+                while (hexString.length() < 32) 
+                { 
+                    hexString.insert(0, '0'); 
+                } 
+                return hexString.toString(); 
+            }
+            //H1: {0,1}*→G1*
+            static Element hash1(String str,Pairing pairing)
+            {
+                String shastring = "";
+                try {
+                shastring = toHexString(getSHA(str));
+                } catch (NoSuchAlgorithmException e) {
+                System.out.println(" No such Algorithm exception occurred ");
+                e.printStackTrace();
+                }
+                byte [] shatringbyte = shastring.getBytes();
+
+                Element g1 = pairing.getG1().newElement().setFromHash(shatringbyte, 0, shatringbyte.length);
+                return g1.duplicate();
+            }
+
     @GetMapping(value = "/ePPK/{identifier}",  produces = "application/json")
     public ResponseEntity<PartialPrivateKey> PartialPrivatekey(@PathVariable(value = "identifier") String identifierString ) throws IOException {
 
