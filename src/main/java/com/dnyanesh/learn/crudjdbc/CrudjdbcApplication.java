9af353c322e2c7a5f45ca78cf6b2ad1a93f31f9a
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-04-01 22:56:04 +0530

    🧘🧘 Letting know spring boot application that we added documentation open api. 🧘🧘

--- src/main/java/com/dnyanesh/learn/crudjdbc/CrudjdbcApplication.java
+++ src/main/java/com/dnyanesh/learn/crudjdbc/CrudjdbcApplication.java
@@ -5,7 +5,10 @@ package com.dnyanesh.learn.crudjdbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class CrudjdbcApplication {
 
 	public static void main(String[] args) throws Exception {
