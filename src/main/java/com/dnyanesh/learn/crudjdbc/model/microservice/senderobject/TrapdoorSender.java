700e8abcb2271538fc9e4fca7ee6dca8a212e229
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-04-03 03:32:43 +0530

    ⛲⛲ Remove array and added arraylist for json  parsing. ⛲⛲

--- src/main/java/com/dnyanesh/learn/crudjdbc/model/microservice/senderobject/TrapdoorSender.java
+++ src/main/java/com/dnyanesh/learn/crudjdbc/model/microservice/senderobject/TrapdoorSender.java
@@ -1,16 +1,17 @@
 package com.dnyanesh.learn.crudjdbc.model.microservice.senderobject;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class TrapdoorSender {
     
             String T1;
             String T2;
             String T3;
-            byte [] T2byte;
-            byte [] T3byte;
+            ArrayList <Byte> T2byte;
+            ArrayList <Byte> T3byte;
             long requiredTime;
-            public TrapdoorSender(String t1, String t2, String t3, byte[] t2byte, byte[] t3byte, long requiredTime) {
+            public TrapdoorSender(String t1, String t2, String t3, ArrayList<Byte> t2byte, ArrayList<Byte> t3byte,
+                    long requiredTime) {
                 T1 = t1;
                 T2 = t2;
                 T3 = t3;
@@ -38,16 +39,16 @@ public class TrapdoorSender {
             public void setT3(String t3) {
                 T3 = t3;
             }
-            public byte[] getT2byte() {
+            public ArrayList<Byte> getT2byte() {
                 return T2byte;
             }
-            public void setT2byte(byte[] t2byte) {
+            public void setT2byte(ArrayList<Byte> t2byte) {
                 T2byte = t2byte;
             }
-            public byte[] getT3byte() {
+            public ArrayList<Byte> getT3byte() {
                 return T3byte;
             }
-            public void setT3byte(byte[] t3byte) {
+            public void setT3byte(ArrayList<Byte> t3byte) {
                 T3byte = t3byte;
             }
             public long getRequiredTime() {
@@ -58,9 +59,8 @@ public class TrapdoorSender {
             }
             @Override
             public String toString() {
-                return "TrapdoorSender [T1=" + T1 + ", T2=" + T2 + ", T2byte=" + Arrays.toString(T2byte) + ", T3=" + T3
-                        + ", T3byte=" + Arrays.toString(T3byte) + ", requiredTime=" + requiredTime + "]";
+                return "TrapdoorSender [T1=" + T1 + ", T2=" + T2 + ", T2byte=" + T2byte + ", T3=" + T3 + ", T3byte="
+                        + T3byte + ", requiredTime=" + requiredTime + "]";
             }
-            
             
 }
